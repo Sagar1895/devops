34e34f45c84864297ba76fdd474714cae0d92641
Sagaronkar <sagarco96@gmail.com>
2022-03-14 02:36:29 +0000

    added git cmds

---
+++ gitcmds.sh
@@ -0,0 +1,9 @@
+Git init: this is used to intialize the git repository.
+	  this cmd is used to convert folder to git repository or folder.
+	  Git status : it is used to check wheather the files are in workspace or staging area or git repo.
+	  Git add . : it is used to move the files from workspace to the staging area.
+	  git commit -m " mesage" :it will move the files from staging area to local git repo.
+
+	  configuring user: git config --global user.name "username"
+	  		  git config --global user.email "mailid"
+			  git push origin master
